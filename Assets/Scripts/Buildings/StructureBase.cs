d833a2d293eed52beedec18fcdee091541822d0e
David Woodhouse <dwoodhouse22@gmail.com>
2022-03-04 14:09:59 +0000

    Can rotate structures in 30deg increments

--- Assets/Scripts/Buildings/StructureBase.cs
+++ Assets/Scripts/Buildings/StructureBase.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 
 namespace TimberCottage.Pathfinding
 {
+    /// <summary>
+    /// Base class for all structures in the game
+    /// </summary>
     public class StructureBase : MonoBehaviour
     {
         [SerializeField] private Transform bottomLeft;
+        [SerializeField] private Transform structureMesh;
+        
         public Vector3 BottomLeft => bottomLeft.position;
         public Vector3 Extents => _boxCollider.size;
         public event Action<bool> OnCollisionEvent;
 
         private HashSet<Collider> _collisions;
-        private bool _constructed = false;
+        private bool _constructed;
         private BoxCollider _boxCollider;
+        private readonly float _rotationStep = 30f;
+        
         private void Awake()
         {
             _collisions = new HashSet<Collider>();
@@ -46,11 +53,41 @@ namespace TimberCottage.Pathfinding
             }
         }
 
+        /// <summary>
+        /// Called when structure is placed into the world
+        /// </summary>
         public void OnConstructed()
         {
-            Debug.Log($"{name} constructed");
             _constructed = true;
             _collisions = null;
+        }
+
+        private void Update()
+        {
+            if (_constructed)
+            {
+                return;
+            }
+            
+            // TODO: move this logic to construction manager
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Rotate(1);
+            }
+        }
+
+        /// <summary>
+        /// Rotate this structure during construction
+        /// </summary>
+        /// <param name="direction">Should be -1 or +1</param>
+        public void Rotate(int direction)
+        {
+            if (_constructed)
+            {
+                return;
+            }
+            
+            structureMesh.transform.rotation *= Quaternion.Euler(0f, _rotationStep * direction, 0f); 
         }
     }
 }
